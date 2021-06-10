8070d16ce26b8a6c475c3646eb2b8ba3fba33730
shanu3742 <krshanu.95@gmail.com>
2021-06-10 04:43:03 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/main.scss';
+import 'rsuite/lib/styles/themes/dark/index.less';
+import { Switch, Route } from 'react-router';
+
+function App() {
+  return (
+    <Switch>
+      <Route>hey</Route>
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config= {
+    apiKey: "AIzaSyBsagCNiTI2eGgxg5dmNSBKzA8tlT0dJ2M",
+    authDomain: "chat-web-app-5855a.firebaseapp.com",
+    projectId: "chat-web-app-5855a",
+    storageBucket: "chat-web-app-5855a.appspot.com",
+    messagingSenderId: "714973422891",
+    appId: "1:714973422891:web:b7e61c2b0bf1c6a63bd16e"
+  };
+
+  const app =firebase.initializeApp(config);
