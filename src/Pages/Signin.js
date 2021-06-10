8242fa2d37509d36cf9618041981cd1bc2fe975d
shanu3742 <krshanu.95@gmail.com>
2021-06-10 16:33:57 -0700

    signin added with google and facebook

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,7 +1,31 @@
 import React from 'react';
-import { Container, Grid, Row, Col, Panel, Button, Icon } from 'rsuite';
+import firebase from 'firebase/app';
+import { Container, Grid, Row, Col, Panel, Button, Icon, Alert } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const Signin = () => {
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed in', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
   return (
     <Container>
       <Grid className="mt-page">
@@ -14,11 +38,11 @@ const Signin = () => {
               </div>
 
               <div className="mt-3">
-                <Button block color="blue">
+                <Button block color="blue" onClick={onFacebookSignIn}>
                   <Icon icon="facebook" />
                   Continue with Facebook
                 </Button>
-                <Button block color="red">
+                <Button block color="red" onClick={onGoogleSignIn}>
                   <Icon icon="google" /> Continue with Google
                 </Button>
               </div>
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,12 +1,16 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database'
 
-const config= {
-    apiKey: "AIzaSyBsagCNiTI2eGgxg5dmNSBKzA8tlT0dJ2M",
-    authDomain: "chat-web-app-5855a.firebaseapp.com",
-    projectId: "chat-web-app-5855a",
-    storageBucket: "chat-web-app-5855a.appspot.com",
-    messagingSenderId: "714973422891",
-    appId: "1:714973422891:web:b7e61c2b0bf1c6a63bd16e"
-  };
+const config = {
+  apiKey: 'AIzaSyBsagCNiTI2eGgxg5dmNSBKzA8tlT0dJ2M',
+  authDomain: 'chat-web-app-5855a.firebaseapp.com',
+  projectId: 'chat-web-app-5855a',
+  storageBucket: 'chat-web-app-5855a.appspot.com',
+  messagingSenderId: '714973422891',
+  appId: '1:714973422891:web:b7e61c2b0bf1c6a63bd16e',
+};
 
-  const app =firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database()
