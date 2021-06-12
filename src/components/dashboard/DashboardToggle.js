f906573c71def3fefe46a293f4e148785201dffd
shanu3742 <krshanu.95@gmail.com>
2021-06-13 04:25:55 +0530

    media querry is added for responsiveness

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { Button, Icon, Drawer } from 'rsuite';
 import Dashboard from '.';
 import useModelState from '../../misc/custom-hooks';
+import { useMediaQuery } from '../../misc/usemediaquery';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModelState();
+  const isMobile = useMediaQuery('(max-width:992px)');
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" />
         Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
