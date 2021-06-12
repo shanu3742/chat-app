1cfad7e352d79e6a223ed5e4d41c4c109b76d27a
shanu3742 <krshanu.95@gmail.com>
2021-06-13 05:06:31 +0530

    dynamically added name is added in sidebar

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Drawer, Button, Icon } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
 const Dashboard = () => {
+  // eslint-disable-next-line no-unused-vars
+  const Profile = useProfile();
+
   return (
     <>
       <Drawer.Header>
         <Drawer.Title>Dashboard</Drawer.Title>
       </Drawer.Header>
       <Drawer.Body>
-        <h3>Hey</h3>
+        <h3>Hey,{Profile.profile.name}</h3>
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red">
