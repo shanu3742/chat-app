7eee704f4d415e2dfe97130dab3b31c77f3c0be8
shanu3742 <krshanu.95@gmail.com>
2021-06-13 04:51:28 +0530

    Sidebar basic Content is added

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Drawer, Button, Icon } from 'rsuite';
 
 const Dashboard = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body>
+        <h3>Hey</h3>
+      </Drawer.Body>
+      <Drawer.Footer>
+        <Button block color="red">
+          <Icon icon="sign-out" /> Signout
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
