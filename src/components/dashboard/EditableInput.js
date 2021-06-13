c60aee6fb46a0a423a40b64a6cc0a0ba986cd1ac
shanu3742 <krshanu.95@gmail.com>
2021-06-14 03:40:44 +0530

    created reusable editable input

---
+++ src/components/dashboard/EditableInput.js
@@ -0,0 +1,69 @@
+import React, { useCallback, useState } from 'react';
+import { Input, InputGroup, Icon, Alert } from 'rsuite';
+
+const EditableInput = ({
+  initialValue,
+  onSave,
+  label = null,
+  placeholder = 'write your value',
+  // eslint-disable-next-line no-unused-vars
+  emptyMsg = 'input is empty',
+  ...inputProps
+}) => {
+  const [onSaveHover, setonSaveHover] = useState(false);
+  const [input, setInput] = useState(initialValue);
+  const onInputChange = useCallback(value => {
+    setInput(value);
+    setonSaveHover(false);
+  }, []);
+  const [isEditable, setIsEditable] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [hover, setHover] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+
+  const onEditClick = useCallback(() => {
+    setIsEditable(p => !p);
+    setInput(initialValue);
+    setHover(true);
+  }, [initialValue]);
+
+  const onSaveClick = async () => {
+    const terimmed = input.trim();
+    if (terimmed === '') {
+      Alert.info(emptyMsg, 4000);
+    }
+    if (terimmed !== initialValue) {
+      await onSave(terimmed);
+    }
+    setonSaveHover(true);
+    setIsEditable(false);
+    setHover(false);
+  };
+  return (
+    <div>
+      {label}
+      <InputGroup>
+        <Input
+          {...inputProps}
+          disabled={!isEditable}
+          placeholder={placeholder}
+          value={input}
+          onChange={onInputChange}
+        />
+        <InputGroup.Button color={hover ? 'red' : 'blue'} onClick={onEditClick}>
+          <Icon icon={isEditable ? 'close' : 'edit2'} />
+        </InputGroup.Button>
+        {isEditable && (
+          <InputGroup.Button
+            color={!onSaveHover ? 'red' : 'green'}
+            onClick={onSaveClick}
+          >
+            <Icon icon="check" />
+          </InputGroup.Button>
+        )}
+      </InputGroup>
+    </div>
+  );
+};
+
+export default EditableInput;
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Drawer, Button, Icon } from 'rsuite';
+import { Drawer, Button, Icon, Divider } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from './EditableInput';
 
 const Dashboard = ({ onSignOut }) => {
   // eslint-disable-next-line no-unused-vars
   const Profile = useProfile();
+  const onSave = async newdata => {
+    console.log(newdata);
+  };
 
   return (
     <>
@@ -13,6 +17,13 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey,{Profile.profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={Profile.profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
