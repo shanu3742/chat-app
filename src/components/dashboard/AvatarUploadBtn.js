f81ae103a04581e1a339845bca8af64e3fc975e3
shanu3742 <krshanu.95@gmail.com>
2021-06-15 02:19:56 +0530

    avtar is created

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button, Alert } from 'rsuite';
+import AvatarEditor from 'react-avatar-editor';
+import { useProfile } from '../../context/profile.context';
+import useModelState from '../../misc/custom-hooks';
 
 const AvatarUploadBtn = () => {
+  const Profile = useProfile();
   const fileInputType = '.png, .jpeg , .jpg';
+  const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
+  const isValidFile = file => acceptedFileTypes.includes(file.type);
+  const { isOpen, open, close } = useModelState();
+  // eslint-disable-next-line no-unused-vars
+  const [img, setImg] = useState(null);
+  const onFileInputChange = eve => {
+    const currFiles = eve.target.files;
+    if (currFiles.length === 1) {
+      const file = currFiles[0];
+      if (isValidFile(file)) {
+        setImg(file);
+        open();
+      } else {
+        Alert.warning(
+          `Hey ${Profile.profile.name} you selected wrong file type ${file.type}`,
+          4000
+        );
+      }
+    }
+  };
   return (
     <div className="mt-3 text-center">
       <div>
@@ -15,8 +40,33 @@ const AvatarUploadBtn = () => {
             type="file"
             className="d-none"
             accept={fileInputType}
+            onChange={onFileInputChange}
           />
         </label>
+        <Modal show={isOpen} onHide={close}>
+          <Modal.Header>
+            <Modal.Title>Adjust and upload new avatar</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="d-flex justify-content-center align-items-center h-100">
+              {img && (
+                <AvatarEditor
+                  image={img}
+                  width={200}
+                  height={200}
+                  border={10}
+                  borderRadius={100}
+                  rotate={0}
+                />
+              )}
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button block appearance="ghost">
+              Upload new avatar
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   );
