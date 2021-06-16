649795ffcffc2fdbbf1cc0d1e1e2749acef3a35b
shanu3742 <krshanu.95@gmail.com>
2021-06-16 13:01:17 -0700

    realtime time is added

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import TimeAgo from 'timeago-react';
 
 const RoomItem = () => {
-  return <div>ghjj</div>;
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-item-center">
+        <h3 className="text-disappear">Room name</h3>
+        <TimeAgo datetime={new Date()} />
+      </div>
+      ;
+      <div className="d-flex">
+        <span>No message yet...</span>
+      </div>
+    </div>
+  );
 };
 
 export default RoomItem;
