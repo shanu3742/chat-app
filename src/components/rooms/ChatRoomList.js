7141bb5531b85c9676d4ed902f0090f1848cd864
shanu3742 <krshanu.95@gmail.com>
2021-06-16 17:04:31 -0700

    displayed chat room

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Nav } from 'rsuite';
+import { Link, useLocation } from 'react-router-dom';
+import { Nav, Loader } from 'rsuite';
+
+import { useRooms } from '../../context/room.context';
 import RoomItem from './RoomItem';
 
 const ChatRoomList = () => {
   const math = 100 - 23.5;
-
+  const rooms = useRooms();
+  const location = useLocation();
   return (
     <Nav
       appearance="subtle"
@@ -14,8 +18,23 @@ const ChatRoomList = () => {
       style={{
         height: `${math}vh`,
       }}
+      activeKey={location.pathname}
     >
-      <RoomItem />
+      {!rooms && (
+        <Loader center vertical content="Loading" speed="slow" size="md" />
+      )}
+      {rooms &&
+        rooms.length > 0 &&
+        rooms.map(room => (
+          <Nav.Item
+            componentClass={Link}
+            to={`/chat/${room.id}`}
+            key={room.id}
+            eventKey={`/chat/${room.id}`}
+          >
+            <RoomItem room={room} />
+          </Nav.Item>
+        ))}
     </Nav>
   );
 };
--- src/context/room.context.js
+++ src/context/room.context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { database } from '../misc/firebase';
 import { transformToArrWithId } from '../misc/helper';
 
@@ -19,3 +19,5 @@ export const RoomProvider = ({ children }) => {
   });
   return <RoomContext.Provider value={rooms}>{children}</RoomContext.Provider>;
 };
+
+export const useRooms = () => useContext(RoomContext);
